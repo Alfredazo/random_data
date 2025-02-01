f6a9ad7021474feb947d25a1f7b9ad9ef15505dc
Alfredo <alfredo@joinneon.com>
2025-02-01 12:04:49 -0600

    Add RandomData class for generating random names and drinks; include requirements.txt

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
 from models.RandomData import RandomData
+
+randomData = RandomData("es_MX")
+
+print("Random name: ", randomData.generate_base_on_quantity(50))
--- models/RandomData.py
+++ models/RandomData.py
@@ -1,4 +1,32 @@
+from faker import Faker
+
+
 class RandomData:
 
-    def __init__(self):
-        pass
+    drinks_dict = {
+        0: "Coca Cola",
+        1: "Pepsi",
+        2: "Fanta",
+        3: "Horchata",
+        4: "Limonada",
+        5: "Agua",
+        6: "Ninguna",
+    }
+
+    def __init__(self, region="en_US"):
+        self.fake = Faker([region])
+
+    def generate_random_name(self):
+        return self.fake.name()
+
+    def generate_random_data_with_drink(self):
+        bebida = self.drinks_dict[self.fake.random_int(0, 6)]
+        return {"nombre:": self.fake.name(), "bebida": bebida}
+
+    def generate_base_on_quantity(self, quantity):
+        lst = []
+        for _ in range(quantity):
+            temp_data = self.generate_random_data_with_drink()
+            print(temp_data)
+            lst.append(temp_data)
+        return lst
